145e7968f51e8c93182255c52f63805299b7f7ea
seoyong-lee <sy3783@gmail.com>
2021-09-06 17:40:03 +0900

    Type test

--- type.ts
+++ type.ts
@@ -3,3 +3,26 @@ let isDone: boolean = false;
 isDone = true;
 
 console.log(typeof isDone); // 'boolean'
+
+// Array
+
+let list: Array<string> = ["a", "b", "c"];
+
+let arr: (number | string)[] = [1, 2, 3, "a"];
+
+// Tuple
+
+let tuple: [number, string];
+
+tuple = [1, "name"];
+
+tuple[1] = "two";
+
+// Enum
+
+enum Team {
+  Manager = 101,
+  Planner = 208,
+  Developer = 302,
+  Designer, // 302 + 1
+}
